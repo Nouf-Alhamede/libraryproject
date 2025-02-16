987ea7b2c0d94bdd18259990e2c1b65575212d9c
Nouf-Alhamede <noof.1598@gmail.com>
2025-02-16 14:34:00 +0300

    int.

--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import apps.bookmodule.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",apps.bookmodule.views.index)
 ]
