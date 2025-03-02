dc6e9a4d70efa98b1b53f881fe3546074156fdb3
Nouf-Alhamede <noof.1598@gmail.com>
2025-03-02 06:00:07 +0300

    lab 4

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin 
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('<int:bookId>/', views.viewbook),
-    path('', views.index),
-    path('index2/<val1>/', views.index2),
-    
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
 ]
+
 
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -8,8 +8,20 @@ def index(request):
 
 # 
 def index2(request, val1):
-    return render(request, "bookmodule/index.html", {"name": f"Value is {val1}"})
+    return render(request,"bookmodule/index.html", {"name": f"Value is {val1}"})
   
+def index(request):
+    return render(request,"bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request,'bookmodule/list_books.html')
+ 
+def viewbook(request, bookId):
+    return render(request,'bookmodule/one_book.html')
+ 
+def aboutus(request):
+    return render(request,'bookmodule/aboutus.html')
+ 
 
 def viewbook(request, bookId):
     # assume that we have the following books somewhere (e.g. database)
